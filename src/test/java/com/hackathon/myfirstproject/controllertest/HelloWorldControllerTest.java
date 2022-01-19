924bd3f8d9dae66d65ee4c45ef45191fd553bb98
MohanaPriyaSs <mohanapriya.17cs@kct.ac.in>
2022-01-19 17:35:56 +0530

    test added

---
+++ src/test/java/com/hackathon/myfirstproject/controllertest/HelloWorldControllerTest.java
@@ -0,0 +1,24 @@
+package com.hackathon.myfirstproject.controllertest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class HelloWorldControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void helloWorldTest() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello World"));
+	}
+}
