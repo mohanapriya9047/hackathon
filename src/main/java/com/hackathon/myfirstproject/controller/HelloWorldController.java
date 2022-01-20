9647741e0483038594b61129d48e4cbb0fd9b98c
mohanapriya9047 <98015446+mohanapriya9047@users.noreply.github.com>
2022-01-20 12:11:37 +0530

    Update HelloWorldController.java

--- src/main/java/com/hackathon/myfirstproject/controller/HelloWorldController.java
+++ src/main/java/com/hackathon/myfirstproject/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String helloWorld() {
 		return "Hello World";
 	}
